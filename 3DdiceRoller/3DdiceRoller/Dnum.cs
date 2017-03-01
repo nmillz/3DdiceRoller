cf6113ffe153e2cff2f582081b375255f9550969
nmillz <pokedude96@yahoo.com>
2017-03-01 09:01:32 -0600

    dnum added

---
+++ 3DdiceRoller/3DdiceRoller/Dnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DdiceRoller
+{
+    class Dnum
+    {
+        int Velocity;
+        int VectorCoords[2] = {0, 0, 0};
+    }
+}
